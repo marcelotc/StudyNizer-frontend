df8b8bbb854396ec57a15d900f5ff92b4b4efd35
Marcelo Cortes <marceloteixeiracortes@gmail.com>
2022-10-18 13:54:12 -0300

    added loading state to login

--- src/pages/session/SignIn/index.jsx
+++ src/pages/session/SignIn/index.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import { Navigate, NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { Input, Button, notification, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
 import { setCalendarDate } from '../../../store/modules/userSession/actions';
-import { Input, Button, notification } from 'antd';
 import { Container, LoginContainer } from './styles';
 import api from '../../../services/api';
 
+const antIcon = <LoadingOutlined style={{ fontSize: 24, color: '#fff' }} spin />;
 
 export const SignIn = ({ signed }) => {
     const [loginPaylod, setLoginPaylod] = useState({
         email: '',
         password: '',
     });
+    const [loading, setLoading] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -21,12 +25,14 @@ export const SignIn = ({ signed }) => {
 
     const handleSignIn = async () => {
         try {
+            setLoading(true);
             const res = await api.post('/user/login', {
                 email: loginPaylod.email,
                 password: loginPaylod.password
             })
             dispatch(setCalendarDate(res?.data));
             localStorage.setItem('@StudyNizer:userSession', JSON.stringify(res?.data));
+            setLoading(false);
         } catch (error) {
             notification.info({
                 message: `${error?.response?.data?.error}`,
@@ -51,7 +57,7 @@ export const SignIn = ({ signed }) => {
                     value={loginPaylod.password} 
                     onChange={(e) => setLoginPaylod({ ...loginPaylod, password: e.target.value }) } 
                 />
-                <Button disabled={textsBlank ? true : false} type='primary' onClick={handleSignIn}>Entrar</Button>
+                <Button disabled={textsBlank ? true : false} type='primary' onClick={handleSignIn}>{!loading ? 'Entrar' : <Spin indicator={antIcon} />}</Button>
                 <NavLink to={'/sign-up'}>
                     <Button>Cadastrar</Button>
                 </NavLink>
